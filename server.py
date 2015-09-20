d884dc4aa07f1c606551f2e8226aa0f89c731ee6
Shane Aston <astonshane@gmail.com>
2015-09-20 17:40:08 -0400

    cleaned up changes; working map demo

--- server.py
+++ server.py
@@ -8,9 +8,5 @@ app.debug = True
 def hello():
     return render_template("map.html")
 
-@app.route("/test")
-def index():
-    return render_template("index.html")
-
 if __name__ == "__main__":
     app.run()
