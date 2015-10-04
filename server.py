4e6cf338641399aeb0baadf05d527d04143717a3
evilpit3 <sawhnp@rpi.edu>
2015-10-03 23:28:42 -0400

    Added prompt using paper-elements

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import render_template
 
-<<<<<<< HEAD
 from event import *
 
 app = Flask(__name__)
@@ -12,14 +11,6 @@ app.debug = True
 def hello():
     return render_template("map.html", events=constructTestEvents())
 
-=======
-app = Flask(__name__)
-app.debug = True
-
-@app.route("/")
-def hello():
-    return render_template("map.html")
->>>>>>> c9e8008da9406cf999e276ff8f7a52624edbe725
 
 if __name__ == "__main__":
     app.run()
