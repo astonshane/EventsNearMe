fdef321de2d4f7cb75d97b77a713e03b573779b6
Shane Aston <astonshane@gmail.com>
2015-10-15 21:27:14 -0400

    flask templates displays Login OR Logout buttons

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask import request
 from flask.ext.pymongo import PyMongo
 from flask import render_template, abort
-
+from flask import session
 
 from event import *
 
@@ -9,9 +10,17 @@ app = Flask("mydb")
 app.debug = True
 mongo = PyMongo(app)
 
+def checkLoggedIn():
+    if request.cookies.get('fbsr_1055849787782314') != None:
+        session['logged_in'] = True
+        session.modified = True
+    else:
+        session['logged_in'] = False
+        session.modified = True
 
 @app.route("/")
 def hello():
+    checkLoggedIn()
     return render_template("map.html", events=constructTestEvents(mongo))
 
 
@@ -34,4 +43,5 @@ def page_not_found(error):
     return render_template('page_not_found.html', choice=choice), 404
 
 if __name__ == "__main__":
+    app.secret_key = 'supersecretsecretkey'
     app.run()
