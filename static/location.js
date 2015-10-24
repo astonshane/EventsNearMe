69f4934f9f7a89287c857dc069ccf97d4f462694
evilpit3 <sawhnp@rpi.edu>
2015-10-23 22:01:22 -0400

    Fixed Marker dist, added cookie with lat,lng

--- static/location.js
+++ static/location.js
@@ -1,30 +1,71 @@
+//Get the user's location info
 $("document").ready(function() {
 		navigator.geolocation.getCurrentPosition(showMap, showError);
-		//bootbox.alert("hi");
+	});
+
+//Make sure all the maps-markers are sperate
+$( window ).load(function() {
+		var mrks = $("google-map-marker");
+		console.log(mrks.length);
+		for(var i = 0; i < mrks.length; i++)
+		{
+			for(var j = i+1; j < mrks.length; j++)
+			{
+				var d = dist(mrks[i],mrks[j]);
+				//console.log(mrks[i].title + "->" + mrks[j].title);
+				//console.log(d);
+				if( d < .01)
+				{
+					//Adjust these according to zoom
+					mrks[j].latitude = Number(mrks[j].latitude) + .0002;
+					mrks[j].longitude = Number(mrks[j].longitude) + .0002; 
+				}
+								
+			}
+		}
 	});
 	
+	//Distance between 2 <google-map-markers>
+	function dist(x,y)
+	{
+		lat1 = x.latitude;
+		lat2 = y.latitude;
+		lon1 = x.longitude;
+		lon2 = y.longitude;
 		
+		var p = 0.017453292519943295;    // Math.PI / 180
+		var c = Math.cos;
+		var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+          c(lat1 * p) * c(lat2 * p) * 
+          (1 - c((lon2 - lon1) * p))/2;
+
+		return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+	}
+	
+	//Call back function for getCurrentPosition, updates <google-map> to user's lat lng	
 	function showMap(position) {
 		var map = document.querySelector('google-map');
 		map.latitude = position.coords.latitude;
 		map.longitude = position.coords.longitude;
-		  
-		$("google-map").append('<google-map-marker latitude=' + position.coords.latitude + ' longitude=' + position.coords.longitude +' title="You"></google-map-marker>'); 
+		
+		document.cookie = "lat:" + position.coords.latitude + ",lng:" + position.coords.longitude;
+		//$("google-map").append('<google-map-marker latitude=' + position.coords.latitude + ' longitude=' + position.coords.longitude +' title="You"></google-map-marker>'); 
 		}
 	
+	//Updates map to inputed lat lng
 	function showMap2(lat,lng) {
 		var map = document.querySelector('google-map');
 		map.latitude = lat
 		map.longitude = lng;
-		  
-		$("google-map").append('<google-map-marker latitude=' + lat + ' longitude=' + lng +' title="You"></google-map-marker>'); 
+		document.cookie = "lat:" + position.coords.latitude + ",lng:" + position.coords.longitude;
+		//$("google-map").append('<google-map-marker latitude=' + lat + ' longitude=' + lng +' title="You"></google-map-marker>'); 
 		}
-		
+	
+	//Callback function for getCurrentPosition, prompt's user to enter location manually
 	function showError(error) {	
-		//Make element visable		
 		bootbox.prompt("Enter your 5 digit zip", function(result) {                
 			if (result === null) {                                             
-				showMap2(37.2350,115.8111);                             
+				showMap2(37.2350,115.8111); //Somewhere in China I think                         
 			} 
 			else {
 				var url = 'http://maps.googleapis.com/maps/api/geocode/json?address=';
