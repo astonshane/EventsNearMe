21091b585bedf0f1a52ef2bdde34767529dc5e35
Shane Aston <astonshane@gmail.com>
2015-10-24 15:03:31 -0400

    styling for datetimepicker; minimum dates

--- static/createEvent.js
+++ static/createEvent.js
@@ -1,12 +1,19 @@
 $(function () {
-    $('#datetimepicker6').datetimepicker();
-    $('#datetimepicker7').datetimepicker({
-        useCurrent: false //Important! See issue #1075
-    });
-    $("#datetimepicker6").on("dp.change", function (e) {
-        $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
-    });
-    $("#datetimepicker7").on("dp.change", function (e) {
-        $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
-    });
+$('#datetimepicker6').datetimepicker({
+  showTodayButton: true,
+  sideBySide: true,
+  minDate: moment()
+});
+$('#datetimepicker7').datetimepicker({
+    useCurrent: false, //Important! See issue #1075
+    showTodayButton: true,
+    sideBySide: true,
+    minDate: moment()
+});
+$("#datetimepicker6").on("dp.change", function (e) {
+    $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+});
+$("#datetimepicker7").on("dp.change", function (e) {
+    $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+});
 });
