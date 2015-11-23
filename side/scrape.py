21a3b89b07865ed0f2c86004b3cb187899697184
Shane Aston <astonshane@gmail.com>
2015-11-23 18:01:17 -0500

    removed some stuff

--- side/scrape.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-url = 'http://events.rpi.edu:7070/feeder/main/eventsFeed.do?f=y&sort=dtstart.utc:asc&fexpr=(categories.href!=%22/public/.bedework/categories/Ongoing%22)%20and%20(entity_type=%22event%22%7Centity_type=%22todo%22)&skinName=list-json&count=10'
-
-f = requests.get(url)
-
-
-print(f.text)
