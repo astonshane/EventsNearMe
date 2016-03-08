45c64f05336d49a6bd64b2d51afc6609db1fb960
Shane Aston <astonshane@gmail.com>
2016-03-08 14:44:27 -0500

    trying to fix markers

--- static/location.js
+++ static/location.js
@@ -63,7 +63,8 @@ function showMap2(lat,lng) {
   map.longitude = lng;
   document.cookie = "lat=" + lat;
   document.cookie = "lng=" + lng;
-  $("google-map").append('<google-map-marker latitude=' + lat + ' longitude=' + lng +' title="You" icon="/static/you.png" zIndex="999"></google-map-marker>');
+  console.log("test");
+  $("google-map").append('<google-map-marker latitude=' + lat + ' longitude=' + lng +' title="You" zIndex="999"></google-map-marker>');
 }
 
 //Callback function for getCurrentPosition, prompt's user to enter location manually
