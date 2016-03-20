f436f2ae4032c41add91379af5038f9c73eaecd6
Shane Aston <astonshane@gmail.com>
2016-03-20 13:32:50 -0400

    create an Admin account from manage.py

--- eventsnearme/manage.py
+++ eventsnearme/manage.py
@@ -1,8 +1,10 @@
 # manage.py
 
-from flask.ext.script import Manager
-
+from flask.ext.script import Manager, prompt, prompt_pass
 from server import app
+from pymongo import MongoClient
+import uuid
+import md5
 
 manager = Manager(app)
 
@@ -17,6 +19,37 @@ def hello(name):
 def createAdmin():
     "Create an Admin User"
     print "Creating Admin User..."
+    fname = prompt(name="First Name")
+    lname = prompt(name="Last Name")
+    email = prompt(name="Email")
+    password = prompt_pass(name="password")
+
+    name = {"last": lname,
+            "first": fname}
+
+    client = MongoClient()
+    db = client.mydb
+    users = db.users
+
+    uid = str(uuid.uuid4())
+    salt = str(uuid.uuid4())
+    m = md5.md5()
+    m.update(unicode(salt)+password)
+    hashed = m.hexdigest()
+
+    admin = {
+        "_id": uid,
+        "hash": hashed,
+        "name": name,
+        "admin": False,
+        "salt": salt,
+        "email": email,
+        "admin": True,
+        "tags": [],
+        "picture": "http://lorempixel.com/g/250/250/"
+    }
+
+    users.insert_one(admin)
 
 if __name__ == "__main__":
     manager.run()
